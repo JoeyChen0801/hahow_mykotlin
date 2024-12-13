3c29f8001cee45cef9ebdb809ee98e186f87aef2
Joey <ufak0029@ms5.hinet.net>
2024-12-13 17:45:28 +0800

    initial commit

---
+++ app/src/main/java/com/tailink81/hahow_mykotlin/Hello.kt
@@ -0,0 +1,16 @@
+package com.tailink81.hahow_mykotlin
+
+class Hello {
+}
+
+fun main(){
+    println("Hello Kotlin!!")
+
+    val age=20
+    println(age.inc());
+    println(age)
+    var name="Joey"
+    println(name.get(1))
+    name="Jack"
+    println(name)
+}
