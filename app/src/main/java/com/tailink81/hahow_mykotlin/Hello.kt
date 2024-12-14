a01900128c19c11b20b63f0f34308f7d9add6e2c
Joey <ufak0029@ms5.hinet.net>
2024-12-14 15:22:34 +0800

    Hello Person

--- app/src/main/java/com/tailink81/hahow_mykotlin/Hello.kt
+++ app/src/main/java/com/tailink81/hahow_mykotlin/Hello.kt
@@ -1,9 +1,15 @@
 package com.tailink81.hahow_mykotlin
 
 class Hello {
+
 }
 
 fun main(){
+
+    val jack=Person()
+    jack.hello();
+    //bmi
+
     println("Hello Kotlin!!")
 
     val age=20
@@ -13,9 +19,13 @@ fun main(){
     println(name.get(1))
     name="Jack"
     println(name)
-
+    val population:Long=9999;
     val weight:Float=65.5f
     val isAdult=false;
     val c='A'
     println(c)
+    //JoeyChen0801
+    //ufak0029@ms5.hinet.net
+    //2jdiojxl630801
+    //https://github.com/JoeyChen0801/hahow_mykotlin.git
 }
---
+++ app/src/main/java/com/tailink81/hahow_mykotlin/Person.kt
@@ -0,0 +1,7 @@
+package com.tailink81.hahow_mykotlin
+
+class Person {
+    fun hello(){
+        println("Hello")
+    }
+}
