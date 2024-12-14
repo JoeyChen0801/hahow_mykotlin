e8674f895ad80112c2fb91cf39a97fc23e1d5a8a
Joey <ufak0029@ms5.hinet.net>
2024-12-14 16:32:43 +0800

    加入建構子

--- app/src/main/java/com/tailink81/hahow_mykotlin/Hello.kt
+++ app/src/main/java/com/tailink81/hahow_mykotlin/Hello.kt
@@ -6,24 +6,34 @@ class Hello {
 
 fun main(){
 
-    val jack=Person()
+    val jack=Person("Joey",92.5f,1.7f)
     jack.hello();
+    //jack.weight=65.6f;
+    //jack.height=1.7f;
+    println(jack.bmi());
+
+    val hank=Person("Hank",70f,1.75f)
+    println(hank.name+","+hank.bmi())
     //bmi
+    val w=92.5f;
+    val h=1.7f;
+    val bmi=w/(h*h);
+    println("bmi:"+bmi);
 
-    println("Hello Kotlin!!")
-
-    val age=20
-    println(age.inc());
-    println(age)
-    var name="Joey"
-    println(name.get(1))
-    name="Jack"
-    println(name)
-    val population:Long=9999;
-    val weight:Float=65.5f
-    val isAdult=false;
-    val c='A'
-    println(c)
+//    println("Hello Kotlin!!")
+//
+//    val age=20
+//    println(age.inc());
+//    println(age)
+//    var name="Joey"
+//    println(name.get(1))
+//    name="Jack"
+//    println(name)
+//    val population:Long=9999;
+//    val weight:Float=65.5f
+//    val isAdult=false;
+//    val c='A'
+//    println(c)
     //JoeyChen0801
     //ufak0029@ms5.hinet.net
     //2jdiojxl630801
--- app/src/main/java/com/tailink81/hahow_mykotlin/Person.kt
+++ app/src/main/java/com/tailink81/hahow_mykotlin/Person.kt
@@ -1,6 +1,15 @@
 package com.tailink81.hahow_mykotlin
 
-class Person {
+class Person (val name:String,val weight:Float, val height:Float) {
+    //var weight:Float=0f;
+    //var height:Float=0f;
+
+    fun bmi():Float
+    {
+        val bmi=weight/(height*height)
+        return bmi
+    }
+
     fun hello(){
         println("Hello")
     }
